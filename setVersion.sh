51a4d1990ec3ef599187c2d1f71cb145f30427bf
Ivan.Miljenovic <Ivan.Miljenovic@gmail.com>
2008-11-01 02:29:50 +1100

    Update setVersion script
    darcs-hash:20081031152950-97e08-438ee0f37bf2a577101d37019ca2172bcda1e01e

--- setVersion.sh
+++ setVersion.sh
@@ -3,7 +3,7 @@
 NewVer=$1
 
 sed -i -e "s/^\(Version:\s*\)\S*$/\1$NewVer/" SourceGraph.cabal
-sed -i -e "s/^\(programVersion = \"\).*\(\"\)/\1$NewVer\2/" Main.hs
+sed -i -e "s/^\(programmeVersion = \"\).*\(\"\)/\1$NewVer\2/" Main.hs
 
 darcs record && darcs tag $NewVer && echo Version set to: $NewVer
 
